45e57f897bc44e2b480060b7530a56b1976a804e
Hooligan-0 <gwen-github@agilack.fr>
2017-05-30 14:28:11 +0200

    = UPD: USB unit-test, add mouse move commands into main loop (more realistic test)

--- usbkey-ios/sw-unit_test/usb/main.c
+++ usbkey-ios/sw-unit_test/usb/main.c
@@ -19,11 +19,16 @@
 #include "usb_desc.h"
 
 static void clk_debug(void);
+static void wait(u32 cycles);
 void mouse_enable(usb_module *mod);
 void mouse_setup (usb_module *mod);
+void mouse_xfer  (usb_module *mod, u8 ep);
 
 usb_module mod;
 usb_class  mouse_class;
+
+int ready;
+u8 ep1[64];
 
 /**
  * @brief Entry point and main loop of the unit-test
@@ -31,6 +36,11 @@ usb_class  mouse_class;
  */
 int main(void)
 {
+	int step;
+	int count;
+
+	ready = 0;
+
 	hw_init();
 
 	usb_init();
@@ -38,14 +48,69 @@ int main(void)
 	memset(&mouse_class, 0, sizeof(usb_class));
 	mouse_class.enable = mouse_enable;
 	mouse_class.setup  = mouse_setup;
+	mouse_class.xfer   = mouse_xfer;
 
 	memset(&mod, 0, sizeof(usb_module));
 	mod.desc  =  mouse_desc_array;
 	mod.class = &mouse_class;
 	usb_config(&mod);
 
+	step = 0;
+	count = 0;
 	while (1)
-		;
+	{
+		/* If HID not ready, nothing to do */
+		if ( ! ready)
+			continue;
+
+		/* Wait a small amount of time */
+		wait(0x40000);
+		/* Set hid-busy flag */
+		ready = 0;
+
+		ep1[0] =    0; // Buttons
+		/* Move right */
+		if (step == 0)
+		{
+			ep1[1] = 0x2; // X
+			ep1[2] = 0x0; // Y
+		}
+		/* Move down */
+		else if (step == 1)
+		{
+			ep1[1] = 0x00; // X
+			ep1[2] = 0x02; // Y
+		}
+		/* Move left */
+		else if (step == 2)
+		{
+			ep1[1] = 0xfe; // X
+			ep1[2] = 0x00; // Y
+		}
+		/* Move up */
+		else if (step == 3)
+		{
+			ep1[1] = 0x00; // X
+			ep1[2] = 0xfe; // Y
+		}
+		/* Do nothing (wait) */
+		else if (step == 4)
+		{
+			ep1[1] = 0x00; // X
+			ep1[2] = 0x00; // Y
+		}
+		ep1[3] =    0; // Wheel
+		usb_transfer(&mod, 0x81, ep1, 4);
+
+		count ++;
+		if (count == 50)
+		{
+			count = 0;
+			step ++;
+		}
+		if (step == 5)
+			step = 0;
+	}
 }
 
 /**
@@ -73,11 +138,18 @@ void mouse_enable(usb_module *mod)
 {
 	/* Enable endpoint 1 for datas */
 	usb_ep_enable(mod, 1, 0x40);
+
+	ready = 1;
 }
 
 void mouse_setup(usb_module *mod)
 {
-	/* Nothing to do :) */
+	usb_transfer(mod, 0x80, 0, 0);
+}
+
+void mouse_xfer(usb_module *mod, u8 ep)
+{
+	ready = 1;
 }
 
 /**
@@ -87,5 +159,16 @@ void mouse_setup(usb_module *mod)
 void USB_Handler(void)
 {
 	usb_irq(&mod);
+}
+
+/**
+ * @brief Wait a short amount of time (simple empty loop)
+ *
+ * @param cycles Number of "nop" loop before return
+ */
+static void wait(u32 cycles)
+{
+	while(cycles)
+		cycles--;
 }
 /* EOF */
